0a83950f48cfad34036cbbdaab85d91a59e2f66f
LislPisl <eroesch@student.unimelb.edu.au>
2019-03-28 16:42:15 +1100

    single shooting for param and init

--- test/feature 3/single-shooting/initial_condition_and_params/params_and_init.jl
+++ test/feature 3/single-shooting/initial_condition_and_params/params_and_init.jl
@@ -1,6 +1,6 @@
 using OrdinaryDiffEq, Plots, Optim, Distributions
 u0 = Float32[2.; 0.]
-datasize = 30
+datasize = 30           # resiudual number! = d's
 tspan = (0.0f0, 3.f0)
 t = range(tspan[1], tspan[2], length = datasize)
 true_A = [-0.1 2.0; -2.0 -0.1]
@@ -23,22 +23,22 @@ function add_noise(in_data, noise_sigma)
     end
     return out_data
 end
-noisy_data = add_noise(ode_data, 0.5)
+noisy_data = add_noise(ode_data, 0.3)
 scatter!(t, noisy_data[1,:], label="noisy data")
 
-
-
-function make_ode(temp_A)
+function make_ode(init_temp_A)
+    temp_u0 = init_temp_A[1,:]
+    temp_A = init_temp_A[2:3,:]
     temp_ODEfunc = param_ODEfunc(temp_A)
-    temp_prob = ODEProblem(temp_ODEfunc, u0, tspan)
+    temp_prob = ODEProblem(temp_ODEfunc, temp_u0, tspan)
     temp_ode_data = Array(solve(temp_prob,Tsit5(),saveat=t))
     return temp_ode_data
 end
 
 
 L2_loss_fct(params) = sum(abs2,noisy_data .- make_ode(params))
-start_params_one = [-0.1 1.0; -0.6 0.01]
-start_params_two = [-0.1 1.0; -0.5 0.01]
+start_params_one = [3.5 0.;-0.1 1.0; -0.6 0.01]
+start_params_two = [1.6 0.1;-0.1 1.0; -0.6 0.01]
 result_one = Optim.optimize(L2_loss_fct, start_params_one )
 result_two = Optim.optimize(L2_loss_fct, start_params_two )
 
@@ -48,7 +48,7 @@ solution_two = make_ode(result_two.minimizer)
 
 solstart_one = make_ode(start_params_one)
 solstart_two = make_ode(start_params_two)
-species =1
+species = 1
 plot(t, ode_data[species,:], label="real sol")
 scatter!(t, noisy_data[species,:], label="train data")
 plot!(t, solstart_one[species,:], label="begin 1 data")
@@ -57,4 +57,6 @@ plot!(t, solstart_two[species,:], label="begin 2 data")
 plot!(t, solution_one[species,:], label="sol 1 data")
 plot!(t, solution_two[species,:], label="sol 2 data")
 
-savefig("0.1diff in third param2species.png")
+savefig("good.png")
+
+#1.5 1.1 does not work --> fist species too far down --> crash. up is ok.
--- test/feature 3/single-shooting/initial_condition_and_params/readme.txt
+++ test/feature 3/single-shooting/initial_condition_and_params/readme.txt
@@ -1,1 +1,10 @@
-here I am trying to infer params and inits.
+Inferring params and inits.
+
+
+motivation: only params
+start_params_one = [-0.1 1.0; -0.6 0.01]  -> fit's nicely
+start_params_two = [-0.1 1.0; -0.5 0.01]  -> crashes
+____
+with inits:
+init of 1.5 1.1 does not work --> fist species too far down --> crash.
+init far up --> ok.
